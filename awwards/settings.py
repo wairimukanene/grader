0b96b988f8a6b603db660d0f77c99340025957e9
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-12 18:35:11 +0300

    commit16-installed bootsrap3

--- awwards/settings.py
+++ awwards/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
+    'bootstrap3',
 ]
 
 MIDDLEWARE = [
