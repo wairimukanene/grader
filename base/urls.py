1ff65d962c1d02da0387f149726079d2b469c9e8
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-12 19:25:13 +0300

    commit26-added apis urls

--- base/urls.py
+++ base/urls.py
@@ -11,5 +11,8 @@ urlpatterns = [
     path('add_rating/<str:pk>/', views.add_ratings, name='add-rating'),
     path('submit_project/', views.submit_project, name='submit-project'),
     path('manage_follow/<str:pk>/', views.manage_follow, name='manage-follow'),
+    path('api/all_profiles/', views.ProfilesList.as_view(), name='api-profiles'),
+    path('api/all_projects/', views.ProjectList.as_view(), name='api-projects'),
+    path('api/all_users/', views.UsersList.as_view(), name='api-users')
 
 ]
