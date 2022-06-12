1b5a0512236244fb433169c46d151eea5c1829cd
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-12 17:31:54 +0300

    added the main.html

--- base/views.py
+++ base/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+def home(request):
+    
+
+    context = { }
+    return render(request, 'base/home.html', context)
