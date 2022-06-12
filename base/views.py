35c31634e8e47ac906970de1dea95716d678883f
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-12 18:58:08 +0300

    commit23-created the register and logout function

--- base/views.py
+++ base/views.py
@@ -64,5 +64,114 @@ def login_page(request):
 
     context={ 'page':page }
     return render(request, 'base/login_register.html', context)
+  
+@login_required(login_url='login')
+def logout_user(request):
+    logout(request)
+    return redirect('home')
+
+def register_user(request):
+    page = 'register'
+    form =  UserRegistrationForm
+
+    try:
+        if request.method == 'POST':
+            form = UserRegistrationForm(request.POST)
+            if form.is_valid():
+                user = form.save(commit=False)
+                user.username = user.username.lower()
+                user.save()
+                login(request, user)
+                messages.success(request, f'Hi {request.user.username}, Your account was successfully created')
+                return redirect('home')
+            else:
+                messages.error(request, 'An error occured during registration, Try again')
+    except Exception as e:
+        messages.error(request,'Something went wrong. Probably a connection issue. Try again')
+
+    context = { 'page':page, 'form':form }
+    return render(request, 'base/login_register.html', context)
+
+def profile(request,pk):
+    user = Profile.objects.get(id=pk)
+    projects = user.project_set.all()
+    followers = user.followers.all()
+    following = user.following.all()
+
+    context = { 'user':user, 'projects':projects, 'followers':followers, 'following':following }
+    return render(request, 'base/profile.html', context)
+
+@login_required(login_url='login')
+def submit_project(request):
+    form = ProjectForm()
+    if request.method == 'POST':
+        try:
+            form = ProjectForm(request.POST, request.FILES)
+            if form.is_valid:
+                user = form.save(commit=False)
+                user.user_project_id = request.user.id
+                user.save()
+                messages.success(request, f'{request.user.username}, Your project was successfully submited')
+                return redirect('home')
+        except Exception as e:
+            messages.error(request, 'An error occured during submition. Try again')
+
+    context = { 'form': form }
+    return render(request, 'base/submit_project_form.html', context)
+
+def project_page(request,pk):
+    project = Project.objects.get(id=pk)
+    ratings = project.rating_set.all()
+    form = RatingForm
+
+    context = {'project':project, 'form':form, 'ratings':ratings}
+    return render(request, 'base/project.html', context)
+
+@login_required(login_url='login')
+def add_ratings(request,pk):
+    project = Project.objects.get(id=pk)
+    ratings = project.rating_set.all()
+    current_user = User.objects.get(id=request.user.id)
+    print(ratings)
+
+    if current_user in ratings:
+        messages.error(request, 'Your rating was already submitted')
+        return redirect('project',pk=pk)
+
+    if request.method == 'POST':
+        try:
+            form = RatingForm(request.POST)
+            if form.is_valid():
+                    rating = form.save(commit=False)
+                    rating.project_id = int(pk)
+                    rating.rated_by_id = request.user.id
+                    rating.save()
+                    messages.success(request, 'Your rating was added successfully')
+                    return redirect('project',pk=pk)
+        except Exception as e:
+                messages.error(request, 'Rating could not be added, Please try again. Ensure your values are between 1 and 10', e) 
+                return redirect('project',pk=pk)
+
+@login_required(login_url='login')
+def manage_follow(request, pk):
+    user_profile = Profile.objects.get(id=pk)
+    all_profiles_followers = user_profile.followers.all()
+    
+    current_user = Profile.objects.get(id=request.user.id)
+    all_current_user_is_following = current_user.following.all()
+
+    if user_profile.id != current_user.id:   
+        if current_user in all_profiles_followers:
+            user_profile.followers.remove(current_user)
+        else:  
+            user_profile.followers.add(current_user) 
+
+        if user_profile in all_current_user_is_following:
+            current_user.following.remove(user_profile)  
+        else:
+            current_user.following.add(user_profile) 
+
+    return redirect('profile',pk=user_profile.id)
+
 
 
