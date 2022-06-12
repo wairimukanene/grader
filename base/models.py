1bf75947c5d37426faa21cf8225b3f2653392091
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-12 18:47:27 +0300

    commit18-created model profile in the models.py

--- base/models.py
+++ base/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from cloudinary.models import CloudinaryField
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user_profile = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = CloudinaryField('profile_photo', blank=True)
+    bio = models.CharField(blank=True, max_length=150)
+    facebook = models.URLField(blank=True, max_length=150)
+    linkedln = models.URLField(blank=True, max_length=150)
+    instagram = models.URLField(blank=True, max_length=150)
+    twitter = models.URLField(blank=True, max_length=150)
+    following = models.ManyToManyField('self', related_name='i_am_following', symmetrical=False, blank=True)
+    followers = models.ManyToManyField('self', related_name='my_followers', symmetrical=False, blank=True)
+    updated = models.DateTimeField(auto_now=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+
+
+
