97486d3cd97f9827f21dd276ac709261adba0f06
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-12 18:56:41 +0300

    commit22-created the login function

--- base/views.py
+++ base/views.py
@@ -1,9 +1,68 @@
 from django.shortcuts import render,redirect
+from .models import Project, Profile
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+from rest_framework.response import Response
+from rest_framework.decorators import APIView
+from .serializers import ProfileSerializer, ProjectSerializer, UserSerializer
+from django.db.models import Q
+from django.contrib import messages 
+from .forms import ProjectForm, UserRegistrationForm, RatingForm
 
 # Create your views here.
+class ProfilesList(APIView):
+    def get(self,request, format=None):
+        profiles = Profile.objects.all()
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
+
+class UsersList(APIView):
+    def get(self,request, format=None):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+        
+class ProjectList(APIView):
+    def get(self,request, format=None):
+        projects = Project.objects.all()
+        serializer = ProjectSerializer(projects, many=True)
+        return Response(serializer.data)
 
 def home(request):
-    
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    projects = Project.objects.filter(
+        Q(title__icontains = q) |
+        Q(description__icontains = q) |
+        Q(user_project__user_profile__username__icontains = q)
+    )
 
-    context = { }
+    context = {'projects': projects }
     return render(request, 'base/home.html', context)
+
+def login_page(request):
+    page = 'login'
+    if request.user.is_authenticated:
+        messages.success(request, 'Already Logged in')
+        return redirect('home')
+
+    if request.method == 'POST':
+        username = request.POST.get('username').lower()
+        password = request.POST.get('password')
+        try:
+            user = User.objects.get(username=username)
+        except Exception as e:
+            messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, f' Welcome back {request.user.username}, we\'ve missed you.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or Password does not exist.')
+
+    context={ 'page':page }
+    return render(request, 'base/login_register.html', context)
+
+
