07199341a984f58d545e01a75c33b17af7f919db
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-12 18:53:47 +0300

    commit21-created the routes for login,logout and register

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,13 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('login/', views.login_page, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
+    path('profile/<str:pk>/', views.profile, name='profile'),
+    path('project/<str:pk>/', views.project_page, name='project'),
+    path('add_rating/<str:pk>/', views.add_ratings, name='add-rating'),
+    path('submit_project/', views.submit_project, name='submit-project'),
+    path('manage_follow/<str:pk>/', views.manage_follow, name='manage-follow'),
+
 ]
