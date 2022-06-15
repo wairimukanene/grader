8dc4690e7de6a28e37c29649a338522ba19f87fb
wairimu.Kanene <mercykanene254@gmail.com>
2022-06-15 12:44:50 +0300

    commit-deployment to heroku

--- awwards/settings.py
+++ awwards/settings.py
@@ -22,7 +22,6 @@ import os
 
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
-DEBUG = config('DEBUG', default=False, cast=bool)
 # development
 if config('MODE')=="dev":
    DATABASES = {
